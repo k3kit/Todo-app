ab94ec54e0c6cbad859691b045a6764191dc74c4
k3kit <hadouken7809@gmail.com>
2022-11-23 18:21:27 +0400

    refactor: add eslint,prettier,firebase and deleted unused files

--- todo/src/firebase.ts
+++ todo/src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJPh68CPggsgBxYVn4Ezof9ArE8fjQh7Q",
-  authDomain: "todo-9682e.firebaseapp.com",
-  projectId: "todo-9682e",
-  storageBucket: "todo-9682e.appspot.com",
-  messagingSenderId: "513404494738",
-  appId: "1:513404494738:web:604b422b8e9a765975974b",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
