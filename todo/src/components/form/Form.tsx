39fd86f9407e176f59af193e87dd46aa9a7ddfa3
k3kit <hadouken7809@gmail.com>
2022-11-28 23:42:31 +0400

    reafactor: add file to task

--- todo/src/components/form/Form.tsx
+++ todo/src/components/form/Form.tsx
@@ -1,11 +1,12 @@
+import { async } from '@firebase/util';
 import { Timestamp } from 'firebase/firestore';
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, useRef, useState } from 'react';
 import { useModal } from '../../hooks/UseModal';
-import { Todo } from '../../pages/main-page/MainPage';
+import { FileType, Todo } from '../../pages/main-page/MainPage';
+import { addTask, uploadFile } from '../../todos.service';
 import { Button } from '../Button/Button';
 import './style.scss';
 interface FormProps {
-  addTask: (task: Todo) => void;
   setFormData?: Todo;
   toggle: () => void;
 }
@@ -18,13 +19,24 @@ const initValue = {
   done: false,
   id: '',
 };
-export const Form: FC<FormProps> = ({ addTask, setFormData = initValue, toggle }) => {
+export const Form: FC<FormProps> = ({ setFormData = initValue, toggle }) => {
   const [task, setTask] = useState(setFormData);
-  const onSubmit = (e: React.SyntheticEvent) => {
+  const [fileStorage, setFileStorage] = useState<File[]>([]);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const onSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    addTask(task);
+    const ddd = await uploadFile(fileStorage);
+    addTask({ ...task, file: ddd[0].url });
+    console.log(ddd);
+
     toggle();
     setTask(initValue);
+  };
+  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFileStorage(Array.from(e.target.files));
+      console.log(Array.from(e.target.files));
+    }
   };
   return (
     <form onSubmit={onSubmit} className="form">
@@ -58,12 +70,10 @@ export const Form: FC<FormProps> = ({ addTask, setFormData = initValue, toggle }
           <input
             placeholder="Add file"
             required
-            name={task.file}
+            ref={fileRef}
             type="file"
-            value={task.file}
-            onChange={(e) => setTask({ ...task, file: e.target.value })}
+            onChange={handleFileUpload}
           />
-          <span className="form__file_span"> add file</span>
         </label>
       </div>
       <Button type={'submit'}>add</Button>
--- todo/src/pages/main-page/MainPage.tsx
+++ todo/src/pages/main-page/MainPage.tsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {
-  collection,
-  getDocs,
-  Timestamp,
-  addDoc,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from 'firebase/firestore';
-import { db } from '../../firebase';
+
 import { Todos } from '../../components/todos/Todos';
 import { Form } from '../../components/form/Form';
 import { Modal } from '../../components/modal/Modal';
 import { useModal } from '../../hooks/UseModal';
 import './style.scss';
 import { Button } from '../../components/Button/Button';
+import { getAllTask } from '../../todos.service';
 
 export type Todo = {
   id: string;
@@ -22,56 +14,20 @@ export type Todo = {
   description: string;
   done: boolean;
   deadline: string;
-  file: string;
+  file?: string;
 };
+export interface FileType {
+  name?: string;
+  url?: string;
+}
 export const MainPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const { isOpen, toggle } = useModal();
-
-  const getAllTask = async () => {
-    const data: Array<Todo> = [];
-    const querySnapshot = await getDocs(collection(db, 'todos'));
-    querySnapshot.forEach((doc) => {
-      data.push({ ...(doc.data() as Todo), id: doc.id });
-    });
-    return data;
-  };
-
-  const addTask = async (task: Todo) => {
-    try {
-      const docRef = await addDoc(collection(db, 'todos'), task);
-      task = { ...task, id: docRef.id };
-      setTodos([...todos, task]);
-    } catch (e) {
-      console.error('Error adding task: ', e);
-    }
-  };
-  const deleteTask = async (id: string) => {
-    try {
-      const docRef = await deleteDoc(doc(db, 'todos', id));
-      setTodos(todos.filter((todo) => todo.id !== id));
-    } catch (e) {
-      console.error('Error delete task: ', e);
-    }
-  };
-
-  const editTask = async (todo: Todo) => {
-    try {
-      await updateDoc(doc(db, 'todos', todo.id), { ...todo });
-      setTodos(
-        todos.map((taskCurrent) => (taskCurrent.id === todo.id ? { ...todo } : taskCurrent))
-      );
-    } catch (e) {
-      console.error('Error edit task: ', e);
-    }
-  };
-
+  const [file, setFile] = useState<FileType[]>([]);
   useEffect(() => {
-    async function fetchData() {
-      const response = await getAllTask();
-      setTodos(response);
-    }
-    fetchData();
+    const subscribeToTodos = getAllTask(setTodos);
+    const unsubscribe = subscribeToTodos();
+    return unsubscribe;
   }, []);
 
   return (
@@ -83,10 +39,10 @@ export const MainPage = () => {
       </header>
       <div>
         <Modal isOpen={isOpen} toggle={toggle}>
-          <Form addTask={addTask} toggle={toggle} />
+          <Form toggle={toggle} />
         </Modal>
       </div>
-      <Todos todos={todos} onDelete={deleteTask} onEdit={editTask} />
+      <Todos todos={todos} />
     </div>
   );
 };
